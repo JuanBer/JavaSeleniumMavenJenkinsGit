8dddc6f28ea0aa97f7997994ccfdf4d5d57e5a9d
JuanBer <jm_bertoni@yahoo.com.ar>
2020-03-23 22:44:20 -0300

    Adding chromedriver.exe to the repo

--- src/main/java/testcases/TC_001.java
+++ src/main/java/testcases/TC_001.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class TC_001 {
     @Test
     public void testcase1(){
-        System.setProperty("webdriver.chrome.driver", System.getenv("SeleniumChromeDriver"));
+        System.setProperty("webdriver.chrome.driver", "src//main//resources//chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.facebook.com");
         driver.findElementById("email").sendKeys("Hello");
--- src/main/java/testcases/TC_002.java
+++ src/main/java/testcases/TC_002.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class TC_002 {
     @Test
     public void testcase2(){
-        System.setProperty("webdriver.chrome.driver", System.getenv("SeleniumChromeDriver"));
+        System.setProperty("webdriver.chrome.driver", "src//main//resources//chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.facebook.com");
         driver.findElementById("email").sendKeys("Hello");
